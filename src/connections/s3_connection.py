951989b38affa59107e392f26fd3b1dcd002fd6f
kumar7ashutosh <kumarashutoshbtech2023@gmail.com>
2025-12-18 23:53:23 +0530

    connection

---
+++ src/connections/s3_connection.py
@@ -0,0 +1,21 @@
+import boto3
+import pandas as pd
+import os
+import sys
+import logging
+from src.logger import logging
+from io import StringIO 
+
+class s3_operations:
+    def __init__(self,bucket_name,aws_access_key,aws_secret_key,region_name='us-east-1'):
+        self.bucket_name=bucket_name
+        self.s3_client=boto3.client('s3',aws_access_key_id=aws_access_key,aws_secret_access_key=aws_secret_key,region_name=region_name)
+        logging.info("Data Ingestion from S3 bucket initialized")
+        
+    def fetch_file_from_s3(self,file_key)->pd.DataFrame:
+        logging.info(f"Fetching file '{file_key}' from S3 bucket '{self.bucket_name}'...")
+        obj=self.s3_client.get_object(Bucket=self.bucket_name,Key=file_key)
+        df=pd.read_csv(StringIO(obj['Body'].read().decode('utf-8')))
+        logging.info(f"Successfully fetched and loaded '{file_key}' from S3 that has {len(df)} records.")
+        return df
+    
